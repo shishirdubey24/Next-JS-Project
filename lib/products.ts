8ffe7fd67d3e46647f52a7bc3c1a25e461220499
Shishir Dubey <shishirdubey221@gmail.com>
2026-09-21 15:45:10 +0530

    "File strucutre and naming is improved"

--- lib/products.ts
+++
@@ -1,8 +0,0 @@
-
-export async function getProductByCategory(category:string){
-  const products = await fetch(`https://dummyjson.com/products/category/${category}`);
-  if (!products.ok) {
-    throw new Error("Failed to fetch products");
-  }
- 
-}
